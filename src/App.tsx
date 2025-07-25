91a30c1cedcec653bcff59f2bebb0304d8529ae0
asifabbasov <aassifabbasov@gmail.com>
2025-07-25 21:10:29 +0400

    added handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+	const [ticker, setTicker] = useState('')
+	// Обработчик изменения поля "Тикер акции"
+	const handleTickerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		return setTicker(e.target.value)
+	}
+
+	const [quantity, setQuantity] = useState(0)
+	// Обработчик изменения поля "Количество акций"
+	const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setQuantity(Number(e.target.value))
+	}
+
+	const [buyPrice, setBuyPrice] = useState(0)
+	// Обработчик изменения поля "Цена покупки"
+	const handleBuyPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setBuyPrice(Number(e.target.value))
+	}
+	const [dividendYield, setDividendYield] = useState(0)
+	// Обработчик изменения поля "Дивидендная доходность"
+	const handleDividendYieldChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setDividendYield(Number(e.target.value))
+	}
+
+	return (
+		<div className='container'>
+			<h1>Калькулятор дивидендов</h1>
+			<form action=''>
+				<div>
+					<label htmlFor='ticker'>Тикер акции</label>
+					<input value={ticker} onChange={handleTickerChange} />
+					<button>Выбрать акцию</button>
+				</div>
+				<div>
+					<label htmlFor='quantity'>Количество акций</label>
+					<input
+						type='number'
+						value={quantity}
+						onChange={handleQuantityChange}
+					/>
+					<button>Количество</button>
+				</div>
+				<div>
+					<label htmlFor='buyPrice'>Цена покупки</label>
+					<input
+						type='number'
+						value={buyPrice}
+						onChange={handleBuyPriceChange}
+					/>
+					<button>Цена покупки</button>
+				</div>
+				<div>
+					<label htmlFor='dividendYield'>Дивидендная доходность</label>
+					<input
+						type='number'
+						value={dividendYield}
+						onChange={handleDividendYieldChange}
+					/>
+				</div>
+			</form>
+		</div>
+	)
 }
-
-export default App;
